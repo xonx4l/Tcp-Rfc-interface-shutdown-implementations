1020f7305bf0a86edc279848a76c0f71271f0ba2
xonx4l <sk797119@gmail.com>
2025-11-07 02:07:40 +0530

    Basic setup with tun-tap

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
+use std::io;
 
-// code here 
+fn main() -> io::Result<()> {
+   let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+   let mut buf = [0u8; 1504];
+   let nbytes = nic.recv(&mut buf[..])?;
+   eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+   Ok(())
+}
