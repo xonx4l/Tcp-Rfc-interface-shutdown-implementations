ac25231e8465a890e1af149fd8814c0b095a7d70
xonx4l <sk797119@gmail.com>
2025-11-13 23:41:09 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ use std::io;
 fn main() -> io::Result<()> {
    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
    let mut buf = [0u8; 1504];
+   loop {
    let nbytes = nic.recv(&mut buf[..])?;
-   eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
-   Ok(())
+   let flags = u16::from_be_bytes([buf[0], buf[1]]);
+   let proto = u16::from_be_bytes((buf[2], buf[3]));
+   eprintln!(
+      "read {} bytes (flags: {:x}, proto: {:x}:) {:x?})",
+      nbytes -4,
+      flags,
+      proto,
+      &buf[4..nbytes]
+    );
+  }
+  Ok(())
+   
 }
