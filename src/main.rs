fe5ae00944196e3c1be1cd4f3c9b1ce932fe1850
xonx4l <sk797119@gmail.com>
2025-11-17 20:37:10 +0530

    main.rs cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,30 @@ fn main() -> io::Result<()> {
    let mut buf = [0u8; 1504];
    loop {
    let nbytes = nic.recv(&mut buf[..])?;
-   let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+   let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
    let eth_proto = u16::from_be_bytes((buf[2], buf[3]));
    if eth_proto != 0x0800 {
        continue;
    }
    
    match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
-    ok(p) => {
+    Ok(p) => {
       let src = p.source_addr();
       let dst = p.destination_addr();
       let proto = p.protocol();
-      eprintln!("got {} bytes of ipv4 packet payload", p.payload_len(),);
+      eprintln!("{} -> {} {}b of protocol {:x}", 
+               src,
+               dst,
+               p.payload_len(),
+               proto,
+              );
       
   }
   Err(e) => {
      eprintln!("ignoring weird packet {:?}, e");
+   }
   }
  }
-}
-  Ok(())
+
    
 }
