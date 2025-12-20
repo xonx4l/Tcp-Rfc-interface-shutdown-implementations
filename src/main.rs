6493082bf1e6bea76eb4d42a76d3559ee51f5688
xonx4l <sk797119@gmail.com>
2025-12-20 23:55:58 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,15 @@ fn main() -> io::Result<()> {
           connections.entry(Quad{
             src: (src ,p.source_port()),
             dst: (dst, p.destination_port()),
-          }).or_default().on_packet(iph, tcph, &buf[datai..nbytes])?;
+          }) {
+             Entry::Occupied(c) => {
+
+             }
+             Entry::Vacant(e) => {
+              
+             }
+          }
+          .or_default().on_packet(iph, tcph, &buf[datai..nbytes])?;
           eprintln!(
             "{} -> {} {}b of tcp port {}", 
                src,
